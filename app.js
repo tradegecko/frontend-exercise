6392a6d207100c874e5fbc12349bcf14d47c03e3
Bradley Priest <bradleypriest@gmail.com>
2014-08-08 17:20:15 +0800

    Move everything to root

---
+++ app.js
@@ -0,0 +1,40 @@
+/*
+    # Endpoint URL #
+
+    https://api.github.com/legacy/repos/search/{query}
+
+    Note: Github imposes a rate limit of 60 request per minute. Documentation can be found at http://developer.github.com/v3/.
+
+    # Example Response JSON #
+
+    {
+      "repositories": [
+        {
+          "created_at": "2014-01-13T02:37:26Z",
+          "description": "A Ruby interface to the TradeGecko API.",
+          "followers": 1,
+          "fork": false,
+          "forks": 2,
+          "has_downloads": true,
+          "has_issues": true,
+          "has_wiki": true,
+          "homepage": null,
+          "language": "Ruby",
+          "name": "gecko",
+          "open_issues": 3,
+          "owner": "tradegecko",
+          "private": false,
+          "pushed": "2014-07-29T08:18:51Z",
+          "pushed_at": "2014-07-29T08:18:51Z",
+          "score": 16.24838,
+          "size": 1617,
+          "type": "repo",
+          "url": "https://github.com/tradegecko/gecko",
+          "username": "tradegecko",
+          "watchers": 1
+        }
+      ]
+    }
+*/
+
+
